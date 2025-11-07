80d4cc159e6d39c3a235645ded9f95dd84bc2306
vishal modanwal <vk0764394@gmail.com>
2025-11-08 01:34:20 +0530

    product filtering feature add

--- src/App.jsx
+++ src/App.jsx
@@ -8,41 +8,73 @@ function App() {
  const [data , setData] = useState([]);
  const [ loading , setLoading] = useState(false);
  const [error , setError] = useState(null);
+ const [ categories , setCategories] = useState([]);
+ const [selectedCategory , setSelectedCategory] = useState("all");
+
 
  const {cart , clearCart} = useCart();
- console.log(cart);
+//  console.log(cart);
+
 
 useEffect(()=>{
-   fetchproducts()
+   fetchCategory();
 } , []);
 
+useEffect(()=>{
+  fetchproducts();
+} , [selectedCategory]);
+
 const fetchproducts = async()=>{
+  const api = selectedCategory ==="all" ? "https://fakestoreapi.com/products" : `https://fakestoreapi.com/products/category/${selectedCategory}`
   setLoading(true);
   try {
-  const response = await fetch("https://fakestoreapi.com/products");
+  const response = await fetch(api);
   const data = await response.json();
   console.log("here" , data);
   setData(data);
-  } catch (error) {
-    console.log("error in fetch Data" , error);
+  } catch (err) {
+    console.log("error in fetch Data" , err);
+    setError("failed to fetch data")
   }finally{
     setLoading(false);
   }
 }
  
+const fetchCategory = async ()=>{
+ try {
+   const response = await fetch("https://fakestoreapi.com/products/categories");
+   const data = await response.json();
+   console.log(data);
+   setCategories(data);
+ } catch (error) {
+    console.log("error to fetching in categories")
+ }
+}
+
 const totalPrice = cart.reduce((a , b)=> a+ b.price ,0);
 
   return (
     <>
       <h1 className="text-2xl font-bold mb-4 text-center">Products</h1>
-      <h2>Cart : {cart.length}</h2>
-      <button  className="mt-2 bg-red-500 text-white px-3 py-1 rounded" onClick={()=>{clearCart()}}>Clear All cart items</button>
-      <p  className="mt-2 bg-amber-300 text-white px-3 py-1 rounded" >Total all Price :{totalPrice} </p>
+      <h2 className='text-xl text-black'>Cart : {cart.length}</h2>
+      <div className='flex justify-around'>
+         <button  className="mt-2 bg-red-500 text-white px-3 py-1 rounded" onClick={()=>{clearCart()}}>Clear All cart items</button>
+      <select value={selectedCategory} onChange={(e)=>{setSelectedCategory(e.target.value)}}>
+        <option value="all">All Products</option>
+      {
+        categories.map((category)=>(
+          <option key={category} value={category}>{category.toUpperCase()}</option>
+         ))
+      }
+      </select>
+      </div>
+     
+      <p  className="mt-2 bg-amber-300 text-balck text-xl px-3 py-1 rounded" >Total all Price :{totalPrice} </p>
 
       {loading ? (
-        <p className="text-center">Loading...</p>
+        <p className="text-center text-2xl text-blue-600">Loading...</p>
       ) : error ? (
-        <p className="text-center text-red-500">Error loading data</p>
+        <p className="text-center text-red-500">{error}</p>
       ) : (
         <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4">
           {data.map((product) => (
--- src/component/Products.jsx
+++ src/component/Products.jsx
@@ -7,9 +7,9 @@ function Products({ data }) {
      
   return (
     <div className="border rounded-2xl p-4 flex flex-col items-center  bg-white max-w-sm mx-auto">
-      <h2 className="text-lg font-semibold text-gray-800 mt-1">{data.title}</h2>
+      <h2 className="text-lg font-semibold text-gray-800 mt-1">{data.title.slice(0,20)+"..."}</h2>
       <p className="text-xl font-bold text-black mt-2">${data.price}</p>
-      <p className="text-sm text-gray-600 text-center mt-2">{data.description}</p>
+      <p className="text-sm text-gray-600 text-center mt-2">{data.description.slice(0 , 25) + "..."}</p>
       <button 
         className="mt-2 bg-black text-white px-3 py-1 rounded"
         onClick={() => addToCart(data)}
